ef459324f33f3af11a28fb3c53e29d608ae27735
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-12-03 18:17:40 -0500

    Fancification 🥳

--- example/index.js
+++ example/index.js
@@ -46,7 +46,11 @@ const makeProjection = (viewportWidth, viewportHeight) =>
 // prettier-ignore
 const lineSimple = createLine(regl, {
   width: 100,
-  color: [0.75, 0.75, 0.75, 1],
+  color: [
+    [0.75, 0.75, 0.75, 1],
+    [0.25, 0.25, 0.25, 1]
+  ],
+  colorIndices: [0, 1, 0, 1],
   is2d: true,
   points: [
     // top line
@@ -73,27 +77,47 @@ const lineSimple = createLine(regl, {
 });
 
 const scale = 20.0;
+const numColors = 256;
 const model = mat4.fromScaling(mat4.create(), [scale, scale, 1.0]);
 
-const line = createLine(regl, { width: 3, miter: 0 });
+const line = createLine(regl, {
+  width: 3,
+  miter: 0,
+  color: Array(numColors)
+    .fill()
+    .map((v, i) => [0.8, 0.5 - (i / 255) * 2, i / 255, 1])
+});
 const extent = 20;
-const numPoints = 200;
+const numPoints = 256;
 const points = polarCurve([], numPoints, t => Math.sin(2.5 * t) * extent);
-const widths = new Array(numPoints)
-  .fill(1)
-  .map((v, i) => ((i + 1) * 4 * extent) / numPoints);
+const widths = new Array(numPoints).fill(1).map((v, i) => i / 2);
+const colorIndices = new Array(numPoints).fill().map((v, i) => i);
 
 // Alter z
 mapElement(points, 2, 3, (v, a, i) => (i / numPoints - 0.5) * extent);
 
-line.setPoints(points, widths);
+line.setPoints(points, { colorIndices, widths });
 
 // Cached for animation
-const pointDataSource = new Float32Array(line.getData().points);
+const pointSource = new Float32Array(line.getData().points);
+const widthSource = new Float32Array(line.getData().widths);
 
 //------------------------------------------------------------------------------
 // Rendering
 //------------------------------------------------------------------------------
+
+const cycleWidths = (buffer, tick) => {
+  const offset = Math.sin(tick * 0.05) / 2 + 1;
+  for (let i = 0; i < buffer.length; i++) {
+    buffer[i] = widthSource[i] * offset;
+  }
+};
+
+const cycleColors = buffer => {
+  for (let i = 0; i < buffer.length; i++) {
+    buffer[i] = (buffer[i] + 1) % numColors;
+  }
+};
 
 regl.frame(({ tick, viewportWidth, viewportHeight }) => {
   regl.clear({
@@ -108,11 +132,17 @@ regl.frame(({ tick, viewportWidth, viewportHeight }) => {
   lineSimple.draw({ projection, model, view });
   // For animation
   mapElement(line.getData().points, 2, 3, (v, a, i) => {
-    const start = pointDataSource[a];
+    const start = pointSource[a];
     const offset = Math.sin(tick * 0.05 + Math.floor(i / 2) * 0.1) * 5;
     return start + offset;
   });
+  // This line ensures that the in-place manipulated `line.getData().points`
+  // is picked up by regl
   line.getBuffer().points.subdata(line.getData().points, 0);
+  cycleWidths(line.getData().widths, tick);
+  line.getBuffer().widths.subdata(line.getData().widths, 0);
+  cycleColors(line.getData().colorIndices, tick);
+  line.getBuffer().colorIndices.subdata(line.getData().colorIndices, 0);
   line.draw({ projection, view });
 });
 
